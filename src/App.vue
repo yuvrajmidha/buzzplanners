1353e15e8fc306b4e2ea8af65ca7266099410946
unknown <inventive8983@gmail.com>
2020-01-20 23:58:56 +0530

    Vue converted

---
+++ src/App.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="app">
+    <nav
+      class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
+      id="ftco-navbar"
+    >
+    <div class="container">
+      <a class="navbar-brand" href="index.html"
+        ><img src="static/images/logo101.svg"  height="40" width="180"></a>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link">Travel</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link">Events</a
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Registration</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <router-view/>
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Travel</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a  class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>Domestic</a
+                  >
+                </li>
+                <li>
+                  <a  class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>International</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>Student Packages</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>Family</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Events</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>Wedding</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span>Corporative</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span
+                    >Student Fest</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span
+                    >Fashion Show</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span
+                    >Social Event</a
+                  >
+                </li>
+                <li>
+                  <a class="py-1 d-block"
+                    ><span class="ion-ios-arrow-forward mr-3"></span
+                    >Exhibitions</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-md">
+            
+          </div>
+          <div class="col-md">
+              <div class="ftco-footer-widget mb-2">
+                <h2 class="ftco-heading-2">Reach Us</h2>
+                <div class="row">
+                    <div class="col-md">
+                        <div class="ftco-footer-widget mb-4">
+                                <ul class="ftco-footer-social list-unstyled float-md-left float-lft ">
+                                  <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                                  <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                                  <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                                </ul>
+                              </div>
+                </div>
+                  </div>
+                </div>
+                
+              </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+   
+      <div
+      class="hero-wrap js-fullheight"
+      style="background-image: url('static/images/back.jpg');"
+    >
+    
+    <div class="container">
+      <div
+        class="row no-gutters slider-text js-fullheight align-items-center justify-content-right"
+        data-scrollax-parent="true"
+      >
+
+          <h1 class="mb-4 pb-1">
+              WE ARE A EVENT AND <br/> TRAVEL PLANNING COMPANY
+                        
+          </h1>
+          <h3 class="mb-4 pb-1">
+            <!-- Submit your presence to the creator of the universe. -->
+          </h3>
+        </div>
+      </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
